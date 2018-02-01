6c0f82d25d94e9c80449510617b94fa5165f22ba
Clement Trosa <me@trosa.io>
2018-02-01 09:39:15 +0100

    Here be Dragons

---
+++ 42devkit.el
@@ -0,0 +1,37 @@
+;;; 42devkit.el --- Development mode for 42 school -*- lexical-binding: t -*-
+
+;; Copyright (c) 2018 Clement T.
+
+;; Author: Clement T. <clement@trosa.io>
+;; Maintainer: Clement T. <clement@trosa.io>
+;; URL: https://github.com/42og/42devkit
+;; Created: January 2018
+;; Keywords: tools, 42, 42devkit
+;; Version:
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; 42devkit: ;; todo write description
+
+;; Some features explained
+
+;;; Code:
+
+(provide '42devkit)
+
+;;; 42devkit.el ends here
