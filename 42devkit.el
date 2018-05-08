57e31ec43409a73598beac96f49ae6e79152ee26
Clement Trosa <me@trosa.io>
2018-05-08 12:18:30 +0200

    Updated norminette wrapper

--- 42devkit.el
+++ 42devkit.el
@@ -40,17 +40,29 @@
 
 ;; Norminette:
 
-(defvar norminette-always-prompt nil
+(defvar norminette--always-prompt nil
   "If the variable is set to true,
    the function will ask to user
    which folder the binary should analyse.
    The default folder where the buffer
    is open.")
 
-(defvar norminette-default-args ""
+(defvar norminette--default-args ""
   "The binary doesn't take options by default,
    but it's possible to specify your own arguments
    by setting up the variable in your scope.")
+
+(defun norminette-ask (p)
+  "Config wrapper to configure the norminette
+   behaviour. The the type arguments are invalid,
+   then ;TODO: he function will ignore it."
+  (cond ((eq t p) (setf norminette--always-prompt t)
+         (eq t nil) (setf norminette--always-prompt nil))))
+
+(defun norminette-args (args)
+  "Configuration wrapper for norminette args"
+  (when (stringp args)
+    (setf norminette--default-args args)))
 
 (defun norminette ()
   "Prompt in a first time user input for project path
@@ -64,17 +76,33 @@
 		 (dir (file-name-directory buffer-file-name))
 		 (greper "| grep -B1 \"^Error\""))
 	(with-output-to-temp-buffer "*Norminette*"
-	  (when (eq norminette-always-prompt t)
+	  (when (eq norminette--always-prompt t)
 		(setq dir (read-file-name "Directory to scan: ")))
-	  (shell-command (format "%s %s %s %s" binary
-                             norminette-default-args dir
+	  (when (eq 1 (shell-command (format "%s %s %s %s" binary
+                             norminette--default-args dir
 							 greper) "*Norminette*" "*Messages*"))
-	(pop-to-buffer "*Norminette*")))
+        (with-local-quit
+          (message "Files in folder are correctly normed. `q` to close")
+          t))
+      (pop-to-buffer "*Norminette*"))))
 
 (defun norminette-file-buffer ()
   "Apply the norminette on the current buffer
    instead of directory"
-  t)
+  (interactive)
+  (let* ((binary "/usr/bin/norminette")
+         (inhibit-read-only nil)
+         (inhibit-quit t)
+		 (dir (buffer-file-name (current-buffer)))
+		 (greper "| grep -B1 \"^Error\""))
+	(with-output-to-temp-buffer "*Norminette File*"
+      (when (eq 1 (shell-command (format "%s %s %s %s" binary
+                                         norminette--default-args dir
+                                         greper) "*Norminette File*" "*Messages*"))
+        (with-local-quit
+          (message "The file is correctly normed. `q` to close")
+          t)
+      (pop-to-buffer "*Norminette File*")))))
 
 (global-unset-key (kbd "M-n"))
 (global-set-key (kbd "M-n")
@@ -82,6 +110,8 @@
 (global-unset-key (kbd "C-x M-n"))
 (global-set-key (kbd "C-x M-n")
                 'norminette-file-buffer) ; Default file binding
+
+;; Norminette ends here
 
 (provide '42devkit)
 
